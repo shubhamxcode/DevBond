1433833e82cd9f24f90d3ac11cedef91d94578b1
shubhamxcode <shubh.varshneycode@gmail.com>
2025-02-07 01:36:55 +0530

    cors update

--- Backend/src/app/app.ts
+++ Backend/src/app/app.ts
@@ -5,7 +5,8 @@ import cookieparser from 'cookie-parser';
 const app = express();
 
 app.use(Cors({
-  origin: 'http://localhost:3000', // Adjust this to your frontend URL
+  origin:process.env.CORS_ORIGIN, // Adjust this to your frontend URL
+  credentials:true,
 }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
