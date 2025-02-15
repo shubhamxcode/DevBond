952f56c21dd36ddc012e66ad5c6e4268e6b0090a
shubhamxcode <shubh.varshneycode@gmail.com>
2025-02-15 19:34:54 +0530

    auth middleware

--- Backend/src/middleware/authenticationToken.ts
+++ Backend/src/middleware/authenticationToken.ts
@@ -1,1 +1,23 @@
+import Apierror from "../utils/apierror";
+import asynchandler from "../utils/asynchandler";
+import Jwt,{JwtPayload} from "jsonwebtoken";
+import { User } from "../models/userschema";
+ const verfiyjwt=asynchandler(async(req,res,next)=>{
+  try {
+     const token= req.cookie?.accessToken||req.header("Authorization")?.replace("Bearer ","")
+     if (!token) {
+      throw new Apierror(401,"unauthorized request")
+     }
+     const decodedToken=Jwt.verify(token,process.env.ACCESS_TOKEN_SECRET as string)
+     const user = await User.findById((decodedToken as JwtPayload)._id);
+     if (!user) {
+      throw new Apierror(401,"invalid excess token")
+     }
+     req.user=user
+     next() 
+  } catch (error) {
+    throw new Apierror(401,"invalid token ")
+  }
+});
 
+export default verfiyjwt
--- Backend/src/routes/route.ts
+++ Backend/src/routes/route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { regiesteruser, loginUser, updateUserField, getUsersByField } from '../controller/user.controlller'
-
+import verfiyjwt from "../middleware/authenticationToken";
 
 const routes = Router();
 
 routes.route("/register").post(regiesteruser);
 routes.route("/login").post(loginUser);
 routes.route("/update-field").post(updateUserField);
-routes.route("/users-by-field").get(getUsersByField);
+routes.route("/users-by-field").get(verfiyjwt,getUsersByField);
 
 
 
